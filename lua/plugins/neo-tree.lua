456d693382c8d9900a60419d56ec96f9943138b7
Victor Medeiros <victoramedeiros@gmail.com>
2024-03-17 15:12:13 -0400

    nvim updates

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,14 +1,30 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	keys = {
-      { "<C-b>", ":Neotree focus left<CR>"},
-      { "<leader>bf", ":Neotree toggle left<CR>"},
+  "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    { "<C-b>", ":Neotree focus left<CR>"},
+    { "<leader>bf", ":Neotree toggle left<CR>"},
     update_focused_file = { enable = true, update_root = true },
   },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
+      },
+    },
+  },
   branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
 }
