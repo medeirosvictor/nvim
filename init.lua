e90fc87f767f83bb72026f626dad83568034e137
Victor Medeiros <victoramedeiros@gmail.com>
2026-02-13 20:34:23 -0500

    Migrate to lazy.nvim with one-file-per-plugin config

--- init.lua
+++ init.lua
@@ -1,34 +1,209 @@
 require("victor.core.options")
 
-vim.pack.add({
-  "https://github.com/nvim-lua/plenary.nvim",
-  "https://github.com/nvim-tree/nvim-tree.lua",
-  "https://github.com/nvim-tree/nvim-web-devicons",
-  "https://github.com/nvim-treesitter/nvim-treesitter",
-  "https://github.com/nvim-telescope/telescope.nvim",
-  "https://github.com/nvim-lualine/lualine.nvim",
-  "https://github.com/akinsho/toggleterm.nvim",
-  "https://github.com/lewis6991/gitsigns.nvim",
-  "https://github.com/neovim/nvim-lspconfig",
-  "https://github.com/williamboman/mason.nvim",
-  "https://github.com/williamboman/mason-lspconfig.nvim",
-  "https://github.com/hrsh7th/nvim-cmp",
-  "https://github.com/hrsh7th/cmp-nvim-lsp",
-  "https://github.com/hrsh7th/cmp-buffer",
-  "https://github.com/hrsh7th/cmp-path",
-  "https://github.com/hrsh7th/cmp-cmdline",
-  "https://github.com/L3MON4D3/LuaSnip",
-  "https://github.com/saadparwaiz1/cmp_luasnip",
-  "https://github.com/ThePrimeagen/harpoon",
-  "https://github.com/folke/which-key.nvim",
-  "https://github.com/mg979/vim-visual-multi",
-  "https://github.com/ellisonleao/gruvbox.nvim",
-}, { load = true })
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-vim.defer_fn(function()
-  require("victor.plugins")
-  require("victor.core.keymaps")
-  require("victor.core.autocmds")
-  require("victor.languages.lsp")
-  require("victor.languages.treesitter")
-end, 0)
+require("lazy").setup({
+  { "nvim-lua/plenary.nvim" },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup({
+        disable_netrw = true,
+        hijack_netrw = true,
+        update_focused_file = { enable = true, update_cwd = false },
+        filters = { custom = { "^.git$" } },
+        git = { enable = true, ignore = true },
+        view = { width = 30, side = "left", signcolumn = "yes" },
+        renderer = { indent_width = 2 },
+      })
+    end,
+  },
+  { "nvim-tree/nvim-web-devicons" },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = { "node_modules", ".git", "dist", "build" },
+          mappings = { i = { ["<esc>"] = require("telescope.actions").close } },
+        },
+        pickers = { find_files = { theme = "dropdown" } },
+      })
+    end,
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup({
+        options = { theme = "gruvbox", component_separators = "|", section_separators = "", globalstatus = true },
+      })
+    end,
+  },
+
+  {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("toggleterm").setup({
+        size = 20,
+        open_mapping = [[<c-t>]],
+        direction = "float",
+        float_opts = { border = "curved" },
+      })
+    end,
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        signs = { add = { text = "│" }, change = { text = "│" }, delete = { text = "_" } },
+        signcolumn = true,
+        on_attach = function(buffer)
+          local gs = package.loaded.gitsigns
+          local function map(mode, l, r, desc)
+            vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+          end
+          map("n", "]h", gs.next_hunk, "Next Hunk")
+          map("n", "[h", gs.prev_hunk, "Prev Hunk")
+          map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+          map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+        end,
+      })
+    end,
+  },
+
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  { "neovim/nvim-lspconfig" },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+      local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        cmp_nvim_lsp.default_capabilities()
+      )
+
+      require("mason-lspconfig").setup({
+        ensure_installed = { "ts_ls", "pyright", "gopls", "clangd", "rust_analyzer" },
+        handlers = {
+          function(server_name)
+            require("lspconfig")[server_name].setup({ capabilities = capabilities })
+          end,
+        },
+      })
+
+      require("mason").setup({ ui = { border = "rounded" } })
+
+      vim.diagnostic.config({ virtual_text = true, signs = true, float = { border = "rounded" } })
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local buf = args.buf
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = buf, desc = "Go to definition" })
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = buf, desc = "Hover" })
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = buf, desc = "Go to references" })
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = buf, desc = "Rename" })
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = buf, desc = "Code action" })
+        end,
+      })
+
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({ { name = "nvim_lsp" }, { name = "luasnip" }, { name = "path" } }, { { name = "buffer" } }),
+      })
+    end,
+  },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+  { "L3MON4D3/LuaSnip" },
+  { "saadparwaiz1/cmp_luasnip" },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua", "vim", "javascript", "typescript", "python", "go", "rust", "html", "css", "json" },
+        sync_install = false,
+        auto_install = true,
+        highlight = { enable = true },
+      })
+    end,
+  },
+
+  {
+    "ThePrimeagen/harpoon",
+    config = function()
+      require("harpoon").setup({})
+    end,
+  },
+
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup({ window = { border = "single" } })
+    end,
+  },
+
+  { "mg979/vim-visual-multi" },
+
+  {
+    "ellisonleao/gruvbox.nvim",
+    config = function()
+      vim.cmd("colorscheme gruvbox")
+    end,
+  },
+}, {
+  defaults = { lazy = false },
+  install = { colorscheme = { "gruvbox" } },
+  checker = { enabled = false },
+  performance = {
+    rtp = {
+      disabled_plugins = { "gzip", "tarPlugin", "tohtml", "tutor", "zipPlugin", "netrwPlugin" },
+    },
+  },
+})
--- lua/victor/plugins/init.lua
+++ lua/victor/plugins/init.lua
@@ -1,138 +1,26 @@
-local plugins = {
-  "https://github.com/nvim-lua/plenary.nvim",
-  "https://github.com/nvim-tree/nvim-tree.lua",
-  "https://github.com/nvim-tree/nvim-web-devicons",
-  "https://github.com/nvim-treesitter/nvim-treesitter",
-  "https://github.com/nvim-telescope/telescope.nvim",
-  "https://github.com/nvim-lualine/lualine.nvim",
-  "https://github.com/akinsho/toggleterm.nvim",
-  "https://github.com/lewis6991/gitsigns.nvim",
-  "https://github.com/neovim/nvim-lspconfig",
-  "https://github.com/williamboman/mason.nvim",
-  "https://github.com/williamboman/mason-lspconfig.nvim",
-  "https://github.com/hrsh7th/nvim-cmp",
-  "https://github.com/hrsh7th/cmp-nvim-lsp",
-  "https://github.com/hrsh7th/cmp-buffer",
-  "https://github.com/hrsh7th/cmp-path",
-  "https://github.com/hrsh7th/cmp-cmdline",
-  "https://github.com/L3MON4D3/LuaSnip",
-  "https://github.com/saadparwaiz1/cmp_luasnip",
-  "https://github.com/ThePrimeagen/harpoon",
-  "https://github.com/folke/which-key.nvim",
-  "https://github.com/mg979/vim-visual-multi",
-  "https://github.com/ellisonleao/gruvbox.nvim",
+return {
+  { "nvim-lua/plenary.nvim" },
+  { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
+  { "nvim-tree/nvim-web-devicons" },
+  { "nvim-treesitter/nvim-treesitter" },
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "nvim-lualine/lualine.nvim" },
+  { "akinsho/toggleterm.nvim" },
+  { "lewis6991/gitsigns.nvim" },
+  { "neovim/nvim-lspconfig", dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" } },
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  { "hrsh7th/nvim-cmp", dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "hrsh7th/cmp-cmdline", "saadparwaiz1/cmp_luasnip", "L3MON4D3/LuaSnip" } },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+  { "L3MON4D3/LuaSnip" },
+  { "saadparwaiz1/cmp_luasnip" },
+  { "ThePrimeagen/harpoon" },
+  { "folke/which-key.nvim" },
+  { "mg979/vim-visual-multi" },
+  { "ellisonleao/gruvbox.nvim" },
+
+  { import = "victor.plugins" },
 }
-
-require("nvim-tree").setup({
-  disable_netrw = true,
-  hijack_netrw = true,
-  auto_close = true,
-  update_focused_file = { enable = true, update_cwd = false },
-  filters = { custom = { "^.git$" } },
-  git = { enable = true, ignore = true },
-  view = {
-    width = 30,
-    side = "left",
-    signcolumn = "yes",
-  },
-  renderer = {
-    indent_width = 2,
-    icons = {
-      web_devicons = {
-        file = { enable = true, color = true },
-      },
-    },
-  },
-})
-
-require("telescope").setup({
-  defaults = {
-    file_ignore_patterns = { "node_modules", ".git", "dist", "build" },
-    mappings = {
-      i = {
-        ["<esc>"] = require("telescope.actions").close,
-      },
-    },
-  },
-  pickers = {
-    find_files = {
-      theme = "dropdown",
-    },
-  },
-})
-
-require("lualine").setup({
-  options = {
-    theme = "gruvbox",
-    component_separators = "|",
-    section_separators = "",
-    globalstatus = true,
-  },
-})
-
-require("toggleterm").setup({
-  size = 20,
-  open_mapping = [[<c-t>]],
-  shade_filetypes = {},
-  shade_terminals = true,
-  shading_factor = "1",
-  start_in_insert = true,
-  persist_size = true,
-  direction = "float",
-  close_on_exit = true,
-  shell = vim.o.shell,
-  float_opts = { border = "curved" },
-})
-
-require("gitsigns").setup({
-  signs = {
-    add = { text = "│" },
-    change = { text = "│" },
-    delete = { _show_top = false, text = "_" },
-    topdelete = { text = "‾" },
-    changedelete = { text = "~" },
-    untracked = { text = "┆" },
-  },
-  signcolumn = true,
-  numhl = false,
-  linehl = false,
-  word_diff = false,
-  watch_gitdir = { interval = 1000, follow_files = true },
-  attach_to_untracked = true,
-  current_line_blame = false,
-  current_line_blame_opts = { virt_text = true, virt_text_pos = "eol", delay = 1000 },
-  current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-  sign_priority = 6,
-  update_debounce = 100,
-  status_formatter = nil,
-  max_file_length = 40000,
-  preview_config = { border = "single", style = "minimal", relative = "cursor", row = 1, col = 1 },
-  on_attach = function(buffer)
-    local gs = package.loaded.gitsigns
-
-    local function map(mode, l, r, desc)
-      vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
-    end
-
-    map("n", "]h", gs.next_hunk, "Next Hunk")
-    map("n", "[h", gs.prev_hunk, "Prev Hunk")
-    map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-    map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-    map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
-    map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
-    map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
-    map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
-    map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
-    map("n", "<leader>ghd", gs.diffthis, "Diff This")
-    map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
-    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
-  end,
-})
-
-require("which-key").setup({
-  window = {
-    border = "single",
-  },
-})
-
-vim.cmd("colorscheme gruvbox")
